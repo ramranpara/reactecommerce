94f44c2de7066847f7393704072d06e312b4ef01
ramranpara <ramranpara@gmail.com>
2019-05-29 23:28:17 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import Details from './components/Details';
+import Default from './components/Default';
 
 function App() {
   return (
     <React.Fragment>
-      <h3>
-        Hello from app
-      </h3>
+      
+      <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/details" component={Details} />
+          <Route component={Default} />
+          
+        </Switch>
     </React.Fragment>
   );
 }
