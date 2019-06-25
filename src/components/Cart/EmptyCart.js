1fb0f8cc11fef9588c59956c8954e92a65ea5abe
ramranpara <ramranpara@gmail.com>
2019-06-25 20:00:32 +0530

    some changes

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EmptyCart() {
+    return (
+        <div className="text-center">
+            <h5>your cart is currently empty....</h5>
+        </div>
+    )
+}
